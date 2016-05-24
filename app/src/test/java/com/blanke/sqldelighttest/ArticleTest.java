64d9754a4642e9c65b51d9dbb8dbd3509b6064e4
blanke-x11 <blankeeee@gmail.com>
2016-05-24 18:32:33 +0800

    add junit test

---
+++ app/src/test/java/com/blanke/sqldelighttest/ArticleTest.java
@@ -0,0 +1,14 @@
+package com.blanke.sqldelighttest;
+
+import org.junit.Test;
+
+/**
+ * Created by blanke on 16-5-24.
+ */
+public class ArticleTest {
+    @Test
+    public void testGetArticle() {
+
+
+    }
+}
